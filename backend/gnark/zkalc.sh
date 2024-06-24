b3588d965b294b04cf30e440a606b6fd2aa811fc
EC2 Default User <ec2-user@ip-172-31-46-251.eu-north-1.compute.internal>
2024-06-24 09:53:32 +0000

    Implementing

--- backend/gnark/zkalc.sh
+++ backend/gnark/zkalc.sh
@@ -4,52 +4,11 @@ zkalc_benchmarks () {
     pushd $1
 
     pushd fr
-
     go test -run none -bench BenchmarkElementAdd
-    go test -run none -bench 'BenchmarkElementMul\b'
-    go test -run none -bench BenchmarkElementInverse
-
-    go test -v -run none -bench BenchmarkElementSquare
-    go test -v -run none -bench BenchmarkElementSqrt
-
     popd
 
-    if [ -d fr/fft ]
-    then
-        pushd fr/fft
-
-        go test -run none -bench BenchmarkFFT
-
-        popd
-    fi
-
-    go test -run none -bench 'BenchmarkG1JacAdd\b'
-    go test -run none -bench 'BenchmarkG1JacScalarMultiplication\b'
-    go test -run none -bench 'BenchmarkMultiExpG1\b'
-
     go test -run none -bench BenchmarkG1JacIsInSubGroup
-    go test -run none -bench BenchmarkG1AffineCofactorClearing
-
-    go test -run none -bench 'BenchmarkG2JacAdd\b'
-    go test -run none -bench 'BenchmarkG2JacScalarMultiplication\b'
-    go test -run none -bench 'BenchmarkMultiExpG2\b'
-
-    go test -run none -bench BenchmarkG2JacIsInSubGroup
-    go test -run none -bench BenchmarkG2AffineCofactorClearing
-
-    if [ -d internal/fptower ]
-    then
-        pushd internal/fptower
-
-        go test -run none -bench BenchmarkE12Mul
-        go test -run none -bench BenchmarkE12Expt
-        # go test -run none -bench BenchmarkE12Cyclosquare
-
-        popd
-    fi
-
-    go test -run none -bench BenchmarkPairing
-    go test -run none -bench BenchmarkMultiPair
+    go test -run none -bench BenchmarkHashToG1
 
     popd
 }
