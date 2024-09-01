975e5e067c001a1922e3008aa43f340f0267ab6c
Mikhail Yakimenko <mikhail.yakimenko123@gmail.com>
2024-09-01 15:19:19 +0200

    Aws machine for secp256k1 bench

--- backend/gnark/zkalc.sh
+++ backend/gnark/zkalc.sh
@@ -5,55 +5,57 @@ zkalc_benchmarks () {
 
     pushd fr
 
-    go test -run none -bench BenchmarkElementAdd
-    go test -run none -bench 'BenchmarkElementMul\b'
-    go test -run none -bench BenchmarkElementInverse
+    #go test -run none -bench BenchmarkElementAdd
+    #go test -run none -bench 'BenchmarkElementMul\b'
+    #go test -run none -bench BenchmarkElementInverse
 
-    go test -v -run none -bench BenchmarkElementSquare
-    go test -v -run none -bench BenchmarkElementSqrt
+    #go test -v -run none -bench BenchmarkElementSquare
+    #go test -v -run none -bench BenchmarkElementSqrt
 
     popd
 
-    if [ -d fr/fft ]
-    then
-        pushd fr/fft
+    # if [ -d fr/fft ]
+    # then
+    #     pushd fr/fft
 
-        go test -run none -bench BenchmarkFFT
+    #     #go test -run none -bench BenchmarkFFT
 
-        popd
-    fi
+    #     popd
+    # fi
 
-    go test -run none -bench 'BenchmarkG1JacAdd\b'
-    go test -run none -bench 'BenchmarkG1JacScalarMultiplication\b'
-    go test -run none -bench 'BenchmarkMultiExpG1\b'
+    # go test -run none -bench 'BenchmarkG1JacAdd\b'
+    # go test -run none -bench 'BenchmarkG1JacScalarMultiplication\b'
+    # go test -run none -bench 'BenchmarkMultiExpG1\b'
 
-    go test -run none -bench BenchmarkG1JacIsInSubGroup
-    go test -run none -bench BenchmarkG1AffineCofactorClearing
+    # go test -run none -bench BenchmarkG1JacIsInSubGroup
+    # go test -run none -bench BenchmarkG1AffineCofactorClearing
 
-    go test -run none -bench BenchmarkHashToG1
+    # go test -run none -bench BenchmarkHashToG1
 
-    go test -run none -bench 'BenchmarkG2JacAdd\b'
-    go test -run none -bench 'BenchmarkG2JacScalarMultiplication\b'
-    go test -run none -bench 'BenchmarkMultiExpG2\b'
+    # go test -run none -bench 'BenchmarkG2JacAdd\b'
+    # go test -run none -bench 'BenchmarkG2JacScalarMultiplication\b'
+    # go test -run none -bench 'BenchmarkMultiExpG2\b'
 
-    go test -run none -bench BenchmarkG2JacIsInSubGroup
-    go test -run none -bench BenchmarkG2AffineCofactorClearing
+    # go test -run none -bench BenchmarkG2JacIsInSubGroup
+    # go test -run none -bench BenchmarkG2AffineCofactorClearing
 
-    go test -run none -bench BenchmarkHashToG2
+    # go test -run none -bench BenchmarkHashToG2
 
-    if [ -d internal/fptower ]
-    then
-        pushd internal/fptower
+    # if [ -d internal/fptower ]
+    # then
+    #     pushd internal/fptower
 
-        go test -run none -bench BenchmarkE12Mul
-        go test -run none -bench BenchmarkE12Expt
-        # go test -run none -bench BenchmarkE12Cyclosquare
+    #     go test -run none -bench BenchmarkE12Mul
+    #     go test -run none -bench BenchmarkE12Expt
+    #     # go test -run none -bench BenchmarkE12Cyclosquare
 
-        popd
-    fi
+    #     popd
+    # fi
 
-    go test -run none -bench BenchmarkPairing
-    go test -run none -bench BenchmarkMultiPair
+    # go test -run none -bench BenchmarkPairing
+    # go test -run none -bench BenchmarkMultiPair
+
+    go test -run none -bench BenchmarkG1AffineBatchScalarMultiplication
 
     popd
 }
